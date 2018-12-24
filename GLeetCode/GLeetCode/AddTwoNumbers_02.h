c30552db237daf1b38d8fc92e0c5ac9750f5b950
525153575 <525153575@qq.com>
2018-12-24 21:53:20 +0800

    add

--- GLeetCode/GLeetCode/AddTwoNumbers_02.h
+++ GLeetCode/GLeetCode/AddTwoNumbers_02.h
@@ -13,7 +13,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface AddTwoNumbers_02 : NSObject
 /**
  给定两个非空链表来表示两个非负整数。位数按照逆序方式存储，它们的每个节点只存储单个数字。将两数相加返回一个新的链表。
- 
+ 2.两数相加，https://leetcode-cn.com/problems/add-two-numbers/
  你可以假设除了数字 0 之外，这两个数字都不会以零开头。
  
  示例：
--- GLeetCode/GLeetCode/TwoNum_01.m
+++ GLeetCode/GLeetCode/TwoNum_01.m
@@ -11,6 +11,7 @@
 @implementation TwoNum_01
 
 /**
+1.两数之和，https://leetcode-cn.com/problems/two-sum/
  给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
  
  你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
